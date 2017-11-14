1cdaad0b0ab0c1801ddb5637f028f5b7feedcded
Colleen Tapen <ctapen@mba2018.hbs.edu>
2017-11-14 04:24:26 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 var name = "Joe"
 var height = "74"
 
-if (name === "Susan" and height === height) {
+if (name === "Susan" & height === "74") {
     console.log("The name is correct")
 } else {
     console.log("Expected the name to be Susan amd height: "+ parseInt(height))
